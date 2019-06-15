7d6bc7c6f56f7ec50a190243b718ed12d3bd5d40
PopSylvestr <kozhin.a.v.n@gmail.com>
2019-06-14 23:29:07 -0400

    display multiple error messages

---
+++ app.rb
@@ -0,0 +1,42 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+
+	@error = 'something wrong!'
+	erb :about
+	end
+	get '/visit' do
+		erb :visit
+	end
+	post '/visit' do
+
+		@username = params[:username]
+		@phone = params[:phone]
+		@datetime = params[:datetime]
+		@barber = params[:barber]
+		@color = params[:color]
+
+
+		hh = { :username => 'Enter your name',
+				:phone => 'Enter your phone',
+				:datetime => 'Enter date and time'
+		}
+
+		
+	@error = hh.select {|key,_| params[key] == ""}.values.join(",")
+
+if @error != ''
+	return erb :visit
+end
+
+		erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+	end
+	def 
